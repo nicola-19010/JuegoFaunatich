4bedebcbe88fe1213dd4f8f438bc8a08a1fef4c7
nicola-19010 <n.pacheco02@ufromail.cl>
2022-10-31 17:03:50 -0300

    nota para nico desde aqui debera construirse txt de animales

--- src/main/java/modelo/MenuJuego.java
+++ src/main/java/modelo/MenuJuego.java
@@ -1,46 +1,7 @@
 package modelo;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class MenuJuego {
-
-    static void mostrarMenu(Jugador jugador) {
-        System.out.println("""
-                Ingrese una opción:
-                1. Empezar.
-                2. Mostrar estadísticas.
-                3. Salir del juego.""");
-        opcionesMenu(jugador);
+    public static void mostrarMenu(Jugador jugadorRegistrado) {
+        System.out.println("Este es el menu");
     }
-    private static int opcionesMenu(int opcionIngresada, Jugador jugador) {
-        switch (opcionIngresada) {
-            case 1 -> Juego.comenzarJuego(jugador);
-            case 2 -> Estadistica.mostrarEstadisticas(coleccionAnimales);
-            case 3 -> salirDelJuego();
-            default -> {
-                System.out.println("Por favor ingrese un número válido:");
-                opcionesMenu(ingresar(), coleccionAnimales);
-            }
-        }
-        return opcionIngresada;
-    }
-    private static int ingresar() {
-        Scanner teclado = new Scanner(System.in);
-        int entrada;
-        do {
-            try {
-                entrada = teclado.nextInt();
-                break;
-            } catch (Exception e) {
-                System.out.println("Por favor ingrese una opción válida:");
-                teclado.nextLine();
-            }
-        } while (true);
-        return entrada;
-    }
-
-
-
 }
